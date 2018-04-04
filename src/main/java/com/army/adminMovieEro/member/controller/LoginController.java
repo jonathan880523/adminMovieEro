b98ba589c4c65f04d49f567154652c4ceade03a8
jonathan880523 <seo880523@gmail.com>
2018-04-04 13:21:14 +0900

    [서진] 세션에 아이디 저장 성공 / 로그인 이외 경로 타고 들어올 경우 로그인페이지로 이동 성공

--- src/main/java/com/army/adminMovieEro/member/controller/LoginController.java
+++ src/main/java/com/army/adminMovieEro/member/controller/LoginController.java
@@ -11,22 +11,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-
-/**
- * Handles requests for the application home page.
- */
 @Controller
 public class LoginController {
 	
 	@Autowired com.army.adminMovieEro.member.model.service.loginService loginService;
-	
 	
 	private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
 	
 	@RequestMapping(value="/")
 	public String home() {
 		logger.info("LoginController 도착.......................");
-		
 		return "main/login";
 	}
 	
@@ -36,21 +30,20 @@ public class LoginController {
 		return "redirect:/";
 	}
 	
-	
 	@RequestMapping(value="login.do", method=RequestMethod.GET)
 	public ModelAndView home(HttpServletRequest request, ModelAndView mv, HttpSession session) {
 		logger.info("LoginController 도착.......................");
 		
-		String ADMIN_ID = request.getParameter("adminId");
-		String ADMIN_PW = request.getParameter("adminPw");
+		String adminId = request.getParameter("adminId");
+		String adminPw = request.getParameter("adminPw");
 		
-		System.out.println("adminId = " + ADMIN_ID);
-		System.out.println("adminPw = " + ADMIN_PW);
+		System.out.println("adminId = " + adminId);
+		System.out.println("adminPw = " + adminPw);
 		
-		boolean checkStatus =  loginService.checkAdmin(ADMIN_ID, ADMIN_PW);
+		boolean checkStatus =  loginService.checkAdmin(adminId, adminPw);
 		if(checkStatus) {
-			session.setAttribute("ADMIN_ID", ADMIN_ID);
-			System.out.println("session : " + session);
+			session.setAttribute("adminId", adminId);
+			System.out.println("session : " + session.getAttribute("adminId"));
 			mv.addObject("ADMIN_ID",session).setViewName("redirect:main.do");
 			return mv;
 		}else {
